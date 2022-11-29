7078b787400c71d84dbfd255cee3c3dbe4f9ce6a
otsembo <okumu.otsembo@gmail.com>
2022-11-29 06:18:11 +0300

    Finished Setup

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Listen for page loading (window)
+
+// TODO: Listen for page loading (document)
+
+// TODO: Access the h1 in HTML page
+
+// TODO: Change color of app_title
+
+// TODO: Change color of app_paragraph
+
+// TODO: Change color of main page
+
+// TODO: Add a new element to the container
+
+// TODO: Remove the h1 from the page
+
+// TODO: Listen for three mouse events
