d0abdcdddad95583d2a59267ee63976ef18ba508
Ian Okumu <okumu.otsembo@gmail.com>
2022-11-30 10:46:56 +0300

    Complete

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const print = (tag, value) => {
+    console.log(tag, value);
+}
+
 // TODO: Listen for page loading (window)
 window.onload = () => {
     console.log('window:', 'loaded');
@@ -5,19 +9,53 @@ window.onload = () => {
 
 // TODO: Listen for page loading (document)
 document.addEventListener('DOMContentLoaded',(e) => {
-    console.log('event',e)
+    // console.log('event',e)
+
+    // TODO: Access the h1 in HTML page
+    const h1 = document.getElementById('app_title');
+    print('h1', h1)
+
+    // TODO: Change color of app_title
+    h1.classList.add('title')
+
+    // TODO: Change color of app_paragraph
+    const p = document.getElementById('app_paragraph');
+
+    p.classList.add('paragraph')
+
+    // TODO: Change color of main page
+    const body = document.querySelector('body')
+
+    body.classList.add('main')
+
+    // TODO: Add a new element to the container
+    const div = document.getElementById('container')
+
+    const header = document.createElement('h2')
+    header.innerText = 'Newly added'
+    
+    div.appendChild(header)
+    // body.appendChild(header)
+
+
+    // TODO: Remove the h1 from the page
+    const b1=document.getElementById('btn-title')
+    const b2 = document.getElementById('btn-paragraph')
+    const b3 = document.getElementById('btn-main')
+
+    b1.addEventListener('click',()=>{
+       h1.remove() 
+    })
+    b2.addEventListener('click',()=>{
+
+    })
+    b3.addEventListener('click',()=>{
+        div.remove()
+    })
+
+    // TODO: Listen for three mouse events (hover, mousedown, mouseenter)
+    document.addEventListener('mousedown', () => {
+        print('mousedown', 'hello')
+    })
+
 })
-
-// TODO: Access the h1 in HTML page
-
-// TODO: Change color of app_title
-
-// TODO: Change color of app_paragraph
-
-// TODO: Change color of main page
-
-// TODO: Add a new element to the container
-
-// TODO: Remove the h1 from the page
-
-// TODO: Listen for three mouse events (hover, mousedown, mouseenter)
